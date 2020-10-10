3d016410f291b999888870fecd565feb8c801a6e
Anthony Giorgi <tonymgiorgi@gmail.com>
2020-10-10 21:42:23 +0300

    setup dynamodb local

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import * as passport from 'passport'
 import * as session from 'express-session'
+import * as busboyBodyParser from 'busboy-body-parser'
 import routes from "./routes"
 import './auth'
 
@@ -12,7 +13,10 @@ class App {
     this.config()
   }
   private config() {
-    this.app.use(bodyParser.json())
+    this.app.use(busboyBodyParser())
+    this.app.use(bodyParser.urlencoded({ extended: true }))
+    this.app.use(bodyParser.json({ strict: false }))
+    this.app.use(bodyParser.text())
     this.app.use(session({ secret: 'SOMETHINGSECRET', resave: false, saveUninitialized: false }))
     this.app.use(passport.initialize())
     this.app.use(passport.session())
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,20 @@
 // src/routes.ts
 import { Router } from "express"
 import * as passport from 'passport'
+import * as AWS from 'aws-sdk'
 
-const router = Router()
+const dynamoDb = new AWS.DynamoDB.DocumentClient(
+  process.env.IS_OFFLINE === 'true'
+    ? {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      }
+    : undefined  
+)
+
+const router = Router({
+  mergeParams: true
+})
 
 router.get("/users", (req, res) => {
   res.json({
@@ -10,6 +22,32 @@ router.get("/users", (req, res) => {
     data: [{
       name: "User1"
     }]
+  });
+})
+
+router.post('/users', function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== 'string') {
+    return res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== 'string') {
+    return res.status(400).json({ error: '"name" must be a string' });
+  }
+
+  const params = {
+    TableName: 'users-table-local',
+    Item: {
+      userId: userId,
+      name: name,
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create user' });
+    } else {
+      res.json({ userId, name });
+    }
   });
 })
 
